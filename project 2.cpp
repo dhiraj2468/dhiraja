f1d1b501ad3df1ff1ebd9f38ce75adf1a5d58d83
dhiraj2468 <137143011+dhiraj2468@users.noreply.github.com>
2023-09-12 22:31:55 +0600

    Update project 2.cpp

--- project 2.cpp
+++ project 2.cpp
@@ -3,12 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 
+#define RED_TEXT "\x1b[31m"
+#define GREEN_TEXT "\x1b[32m"
+#define YELLOW_TEXT "\x1b[33m"
+#define BLUE_TEXT "\x1b[34m"
+#define MAGENTA_TEXT "\x1b[35m"
+#define CYAN_TEXT "\x1b[36m"
+#define RESET_TEXT "\x1b[0m"
+
 int main() {
     int screenWidth = 100;
     char fullName[50];
     int animationSpeed = 1;
 
-    printf("Enter the Your name: ");
+    printf("Enter Your name: ");
     scanf("%s", fullName);
 
     int nameLength = strlen(fullName);
@@ -19,12 +27,34 @@ int main() {
 
         for (int i = 0; i < screenWidth; i++) {
             if (i >= position && i < position + nameLength) {
+                switch (i - position) {
+                    case 0:
+                        printf(RED_TEXT);
+                        break;
+                    case 1:
+                        printf(GREEN_TEXT);
+                        break;
+                    case 2:
+                        printf(YELLOW_TEXT);
+                        break;
+                    case 3:
+                        printf(BLUE_TEXT);
+                        break;
+                    case 4:
+                        printf(MAGENTA_TEXT);
+                        break;
+                    case 5:
+                        printf(CYAN_TEXT);
+                        break;
+                    default:
+                        printf(RESET_TEXT);
+                }
                 printf("%c", fullName[i - position]);
             } else {
                 printf(" ");
             }
         }
-        printf("\n");
+        printf(RESET_TEXT "\n");
 
         position -= animationSpeed;
 
